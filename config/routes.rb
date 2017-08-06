4dd890cff740ae6f8837e3485d32a80ffc28186e
Waldo <w86ccompositor@gmail.com>
2017-08-06 17:40:06 -0400

    New pic file was linked to localhost/3000

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'pictures/pictures'
+
   resources :pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pictures#new'
+
+
 end
